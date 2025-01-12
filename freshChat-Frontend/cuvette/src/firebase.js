20bb9a2b312e5bfddf76aba1ce08bb5cee770d90
GOKU <113460547+KartikAKGEC89@users.noreply.github.com>
2025-01-13 00:17:26 +0530

    Secure secret

--- freshChat-Frontend/cuvette/src/firebase.js
+++ freshChat-Frontend/cuvette/src/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVu0zhmDTb4A4unM2ZSlRP3V_s0NMbDpo",
-  authDomain: "cuvette-task.firebaseapp.com",
-  projectId: "cuvette-task",
-  storageBucket: "cuvette-task.appspot.com",  
-  messagingSenderId: "197753857524",
-  appId: "1:197753857524:web:14c63d6889b912352445a1",
-  measurementId: "G-43MCG39K67"
+ 
 };
 
 const app = initializeApp(firebaseConfig);
